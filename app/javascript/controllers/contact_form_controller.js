79bd764ab7ea7f33dbd4239ae9c287e1fbfa44b7
Maxime Oudin <oudinmaxime@gmail.com>
2023-09-28 09:27:47 +0200

    colors changes for icones and stimulus controller for contact modal created

---
+++ app/javascript/controllers/contact_form_controller.js
@@ -0,0 +1,35 @@
+// app/javascript/controllers/contact_form_controller.js
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["modal", "email", "message"];
+
+  connect() {
+    console.log("hello contact");
+  }
+
+  openModal() {
+    this.modalTarget.classList.add("is-active");
+    console.log("open modal");
+  }
+
+  closeModal() {
+    this.modalTarget.classList.remove("is-active");
+  }
+
+  submit() {
+    const email = this.emailTarget.value;
+    const message = this.messageTarget.value;
+
+    // Ajoutez ici la logique pour envoyer le formulaire (par exemple, une requête AJAX)
+    console.log("Email:", email);
+    console.log("Message:", message);
+
+    // Réinitialiser les champs du formulaire
+    this.emailTarget.value = "";
+    this.messageTarget.value = "";
+
+    // Fermer la modal
+    this.closeModal();
+  }
+}
