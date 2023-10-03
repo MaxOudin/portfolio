5d007b9aaf9c8bf1d6082fa2626ebdaad589c429
Maxime Oudin <oudinmaxime@gmail.com>
2023-10-03 15:11:15 +0200

    creation of project surf

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,6 @@
 class Project < ApplicationRecord
+  has_many_attached :photos
+  def photo_urls
+    photos.map { |photo| Cloudinary::Utils.cloudinary_url(photo.key) }
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+surf = Project.new(
+  title: 'Surf Ai',
+  description: 'Plateforme de mise en relation pour location de planches de surf',
+  date: 'August 2023',
+  ghrepo: 'https://github.com/MaxOudin/surf-ai',
+  tool: 'Ruby On Rails, JavaScript, Devise, Mapbox, Flatpickr'
+)
