032aaf78dbfcfebb88e9abcf1df55a0fb6093f1e
Maxime Oudin <oudinmaxime@gmail.com>
2023-10-04 09:35:32 +0200

    added pictures to project surf

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'open-uri'
 
+# ProjectImage.destroy_all
 Project.destroy_all
 puts 'Former projects deleted.'
 
@@ -18,17 +20,22 @@ surf = Project.new(
 )
 
 photo_urls = [
-  'https://res.cloudinary.com/dyleaesxc/image/upload/v1696342844/Capture_d_e%CC%81cran_2023-10-03_a%CC%80_14.24.04_zdsgwn.png',
-  'https://res.cloudinary.com/dyleaesxc/image/upload/v1696342844/Capture_d_e%CC%81cran_2023-10-03_a%CC%80_14.27.41_vjtbox.png',
-  'https://res.cloudinary.com/dyleaesxc/image/upload/v1696342844/Capture_d_e%CC%81cran_2023-10-03_a%CC%80_14.25.51_tdbjpy.png',
-  'https://res.cloudinary.com/dyleaesxc/image/upload/v1696342844/Capture_d_e%CC%81cran_2023-10-03_a%CC%80_14.26.38_cojluv.png'
+  "https://res.cloudinary.com/dyleaesxc/image/upload/v1696342844/Home_surf.png",
+  "https://res.cloudinary.com/dyleaesxc/image/upload/v1696342844/Show_board_surf.png",
+  "https://res.cloudinary.com/dyleaesxc/image/upload/v1696342844/Confirmation_booking_surf.png",
+  "https://res.cloudinary.com/dyleaesxc/image/upload/v1696342844/Index_booking_surf.png"
 ]
 
+surf.save!
+
 photo_urls.each do |url|
-  project_image = ProjectImage.new(url: url)
-  surf.project_images << project_image
+  file = URI.open(url)
+  # project_image = ProjectImage.new
+  surf.photos.attach(io: file, filename: "nes.png", content_type: "image/png")
+  # surf.project_images << project_image
 end
 
+# project_image.save!
 surf.save!
 puts 'Project "Surf Ai" created with images.'
 puts 'Seed done.'
