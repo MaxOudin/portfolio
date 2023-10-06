0b68b419f8ccfd41b60ba047a751606412cdbff0
Maxime Oudin <oudinmaxime@gmail.com>
2023-10-06 14:43:50 +0200

    script added in head of html

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,21 +3,3 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "@popperjs/core"
 import "bootstrap"
-
-const express = require('express');
-const app = express();
-
-app.use((req, res, next) => {
-  if (req.header('x-forwarded-proto') !== 'https') {
-    res.redirect(`https://${req.header('host')}${req.url}`);
-  } else {
-    next();
-  }
-});
-
-// Rest of your app setup
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
